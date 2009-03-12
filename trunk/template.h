8f76d8358e1cdef86e672d49136515ca5a530b28
geoffrey.anneheim <geoffrey.anneheim@d1d4eeca-0f15-11de-8db3-39f4db58e5fa>
2009-03-12 20:30:49 +0000

    + Added project description in french. + Added template for source code. + Added LPGL licence file.

---
+++ trunk/template.h
@@ -0,0 +1,20 @@
+/****************************************************************************
+ ** Copyright (C) 2009 by ANNEHEIM Geoffrey and PORTEJOIE Julien
+ ** Contact: geoffrey.anneheim@free.fr
+ **
+ ** This file is part of the template of code.
+ **
+ ** GNU General Public License Usage
+ ** Alternatively, this file may be used under the terms of the GNU
+ ** General Public License version 3.0 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.TXT included in the
+ ** packaging of this file.  Please review the following information to
+ ** ensure the GNU General Public License version 3.0 requirements will be
+ ** met: http://www.gnu.org/copyleft/gpl.html.
+ ****************************************************************************/
+ 
+ #ifndef TEMPLATE_H
+ #define TEMPLATE_H
+ 
+ #endif
+ 
