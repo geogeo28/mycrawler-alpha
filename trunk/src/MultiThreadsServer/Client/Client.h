ca42b34dc3f1d4cf28fab6453cf83c8fa29632e1
geoffrey.anneheim <geoffrey.anneheim@d1d4eeca-0f15-11de-8db3-39f4db58e5fa>
2009-04-08 22:26:52 +0000

    / Update svn properties on source code. + Added svn configuration profil (svn properties and ignore pattern configs).

--- trunk/src/MultiThreadsServer/Client/Client.h
+++ trunk/src/MultiThreadsServer/Client/Client.h
@@ -17,7 +17,7 @@
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
  *
- * RCSID $Id: template.h 18 2009-03-28 15:31:25Z geoffrey.anneheim $
+ * RCSID $Id$
  ****************************************************************************/
 
 #ifndef CLIENT_H
